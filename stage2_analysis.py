80be1663bd43f56d2747cdd3effe62e03b2829fd
cjauton <126740758+cjauton@users.noreply.github.com>
2023-06-08 16:06:03 +0900

    added methods to calculate A_LH and dA_LH

--- stage2_analysis.py
+++ stage2_analysis.py
@@ -1,7 +1,5 @@
 import ROOT
-from typing import Type
 import numpy as np
-# from ROOT import TFile, TH1D 
 
 numch = 32
 # input_file = "stage0_output.root"
@@ -44,42 +42,43 @@ hEgam_gate_FA = [6637,6702]; # 127I
 hEgam_gate_FE = [6150,6194]; # 127I
 hEgam_gate_FA_bkg = [6777,7044]; # 127I 
 
-hEn = []
-hEn_canvas = []
-hEgam_canvas = []
-hEgam = []
-hEgam_bkg = []
-hEn_bkg = []
-hEn_bkg_sub = []
-scale = []
+# hEn = []
+# hEn_canvas = []
+# hEgam_canvas = []
+# hEgam = []
+# hEgam_bkg = []
+# hEn_bkg = []
+# hEn_bkg_sub = []
+# scale = []
 
-A_LH_x = []
-A_LH_y = []
-A_LH_y_err = []
+# A_LH_x = []
+# A_LH_y = []
+# A_LH_y_err = []
 
 
-N_L_det = []
-N_L = [0.0] * 7
+# N_L_det = []
+# N_L = [0.0] * 7
 
-N_H_det = []
-N_H = [0.0] * 7
+# N_H_det = []
+# N_H = [0.0] * 7
 
-A_LH_det = [] 
-dA_LH_det = []
-A_LH = [0.0] * 7
-dA_LH = [0.0] * 7
+# A_LH_det = [] 
+# dA_LH_det = []
+# A_LH = [0.0] * 7
+# dA_LH = [0.0] * 7
 
 # det = [1, 2, 3, 6, 7, 9, 10, 11, 13, 15, 17, 18, 19, 25, 26] #127I 
-# det = [1, 2, 3, 6, 7,9,10,11,13,17,18,19,20,26] #111Cd 
-theta_det = [71, 90, 109, 72, 90, 90, 90, 109, 71, 90, 108, 144, 36, 36, 144] #127I
-theta_pos = [-1,71,90,109,109,90,71,90,-1,71,90,109,109,90,71,90,-1,36,72,108,144,-1,-1,-1,-1,144,108,72,36,-1,-1,-1] 
-# theta_det = [71, 90, 109, 72, 90,90,90,109,71,108,144,72,36,144] #111Cd 
-cos_theta_det = [ROOT.TMath.Cos(theta * ROOT.TMath.Pi() / 180) for theta in theta_det]
-theta = [36, 71, 72, 90, 108, 109, 144]
-cos_theta = [0.809017, 0.325568, 0.309017, 0.0, -0.309017, -0.325568, -0.809017]
+# # det = [1, 2, 3, 6, 7,9,10,11,13,17,18,19,20,26] #111Cd 
+# theta_det = [71, 90, 109, 72, 90, 90, 90, 109, 71, 90, 108, 144, 36, 36, 144] #127I
+# theta_pos = [-1,71,90,109,109,90,71,90,-1,71,90,109,109,90,71,90,-1,36,72,108,144,-1,-1,-1,-1,144,108,72,36,-1,-1,-1] 
+# # theta_det = [71, 90, 109, 72, 90,90,90,109,71,108,144,72,36,144] #111Cd 
+# cos_theta_det = [ROOT.TMath.Cos(theta * ROOT.TMath.Pi() / 180) for theta in theta_det]
+# theta = [36, 71, 72, 90, 108, 109, 144]
+# cos_theta = [0.809017, 0.325568, 0.309017, 0.0, -0.309017, -0.325568, -0.809017]
 
 def get_det_map (file_name: str) -> list[int]:
-    """Takes a string of a .root file and returns a list containing the detector map"""
+    """Takes a string of a root file and returns a list containing the detector map"""
+
     file = ROOT.TFile(file_name)
     det_pos_graph = file.Get("det_pos_graph")
     det = []
@@ -92,9 +91,30 @@ def get_det_map (file_name: str) -> list[int]:
     return det
 
 
+def get_det_angle (file_name: str) -> list[int]:
+    """Takes a string of a root file and returns a list containing the detector map"""
 
-def get_hist (file_name: str, hist_name: str) -> list[Type[ROOT.TH1D]]:
-    """Takes a string of a .root file and a name of a histogram and returns a list of histograms"""
+    file = ROOT.TFile(file_name)
+    det_pos_graph = file.Get("det_pos_graph")
+    det_pos = []
+    pos_angle = [-1,71,90,109,109,90,71,90,-1,71,90,109,109,90,71,90,-1,36,72,108,144,-1,-1,-1,-1,144,108,72,36,-1,-1,-1] 
+    det_angle = []
+
+    for i in range(det_pos_graph.GetN()):
+        det_pos.append(int(det_pos_graph.Eval(i)))
+        # print(type(det_pos_graph.Eval(i)))
+
+    file.Close()
+    
+    for i in range(numch):
+        det_angle.append(pos_angle[det_pos[i]])
+
+    return det_angle
+
+
+def get_hist (file_name: str, hist_name: str) -> list[ROOT.TH1]:
+    """Takes a string of a root file and a name of a histogram and returns a list of histograms"""
+
     hist = []
     file = ROOT.TFile(file_name)
     
@@ -107,7 +127,98 @@ def get_hist (file_name: str, hist_name: str) -> list[Type[ROOT.TH1D]]:
     return hist
 
 
+def calc_scale(hEgam_bkg: list[ROOT.TH1]) -> list[float]:
+    """Takes a list of histograms, calculates the scale and return the scales as a list"""
 
+    scale = []
+
+    for i in range(numch):
+        try:     
+            num = hEgam_bkg[i].Integral(hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA[0]),hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA[1]))
+            dem = hEgam_bkg[i].Integral(hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA_bkg[0]),hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA_bkg[1]))
+            scale.append(num/dem)
+        except ZeroDivisionError:
+            scale.append(0)
+    
+    return scale
+
+
+def calc_hEn_sub(hist: list, hist_bkg: list, scale: list[float]) -> list[ROOT.TH1]:
+    """Takes 2 lists of histograms and a list of scales and calculates a new histogram then returns the histograms as a list"""
+
+    hist_bkg_sub = []
+    for i in range(numch):
+        hist_bkg_sub.append(hist[i]-hist_bkg[i]*scale[i])
+        hist_bkg_sub[i].SetDirectory(0)
+
+    return hist_bkg_sub
+
+
+def get_A_LH_det(hist: list[ROOT.TH1],energy: float, width: float) -> tuple[list[float], list[float]]:
+    """Takes a list of histograms and returns a list of A_LH_det and its error dA_LH_det"""
+    
+    N_L_det = []
+    N_H_det = []
+    A_LH_det = []
+    dA_LH_det = []
+    
+
+    for i in range(numch):
+        energy_bin = hist[i].FindBin(energy)
+        width_bin = int(width//hist[i].GetBinWidth(energy_bin))
+        N_L_det.append(hist[i].Integral(energy_bin - width_bin, energy_bin - 1))
+        N_H_det.append(hist[i].Integral(energy_bin, energy_bin + width_bin))
+    
+        try:     
+            A_LH_det.append(calc_A_LH(N_L_det[i],N_H_det[i]))
+            dA_LH_det.append(calc_dA_LH(N_L_det[i],N_H_det[i]))
+        except ZeroDivisionError:
+            A_LH_det.append(-33)
+            dA_LH_det.append(-33)
+
+    return A_LH_det, dA_LH_det
+
+
+def get_A_LH_angle (hist: list[ROOT.TH1], det_angle: list[int], energy: float, width: float) -> tuple[list[float], list[float]]:
+    """Takes a list of histograms and returns a list of A_LH_det and its error dA_LH_det"""
+
+    angle_list = [36,71,72,90,108,109,144]
+    N_L_angle = [0] * len(angle_list)
+    N_H_angle = [0] * len(angle_list)
+    A_LH_angle = [] 
+    dA_LH_angle = []
+
+   
+    for i in range(numch):
+        energy_bin = hist[i].FindBin(energy)
+        width_bin = int(width//hist[i].GetBinWidth(energy_bin))
+        N_L = hist[i].Integral(energy_bin - width_bin, energy_bin - 1)
+        N_H  = hist[i].Integral(energy_bin, energy_bin + width_bin)
+        
+        for j in range(len(angle_list)):
+            if angle_list[j] == det_angle[i]:
+                N_L_angle[j] = N_L_angle[j] + N_L
+                N_H_angle[j] = N_H_angle[j] + N_H
+
+    for j in range(len(angle_list)):
+        try:     
+            A_LH_angle.append(calc_A_LH(N_L_angle[j],N_H_angle[j])) 
+            dA_LH_angle.append(calc_dA_LH(N_L_angle[j],N_H_angle[j]))
+        except ZeroDivisionError:
+            A_LH_angle.append(-33)
+            dA_LH_angle.append(-33)
+
+    return A_LH_angle, dA_LH_angle
+
+def calc_A_LH (N_L: float, N_H: float) -> float:
+    """Takes two floats N_L and N_H and returns A_LH"""
+
+    return (N_L - N_H) / (N_L + N_H)
+
+def calc_dA_LH (N_L: float, N_H: float) -> float:
+    """Takes two floats N_L and N_H and returns dA_LH"""
+
+    return 2 * ROOT.TMath.Sqrt((N_L * N_L * N_H + N_L * N_H * N_H)) / (N_L + N_H) / (N_L + N_H)
 
 
 # file_input = ROOT.TFile(file_name)
@@ -118,54 +229,21 @@ def get_hist (file_name: str, hist_name: str) -> list[Type[ROOT.TH1D]]:
 # hEn_bkg = get_hist(input_file,"hEn_gated_FA_bkg")
 # hEgam = get_hist(input_file,"hEgam")
 
+# hist_canvas = []
 
-def calc_scale(hEgam_bkg: list[ROOT.TH1]) -> list[float]:
-    """Takes a list of histograms, calculates the scale and return the scales as a list"""
-    scale = []
 
-    for i in range(numch):
+# def draw_hists(hist: list[ROOT.TH1], energy: float, width: float) -> None:
 
-        hEgam[i].GetXaxis().SetRangeUser(6500,7100)
-
-        try:     
-            num = hEgam_bkg[i].Integral(hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA[0]),hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA[1]))
-            dem = hEgam_bkg[i].Integral(hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA_bkg[0]),hEgam_bkg[i].GetXaxis().FindBin(hEgam_gate_FA_bkg[1]))
-            scale.append(num/dem)
-        except ZeroDivisionError:
-            scale.append(0)
+#     global hist_canvas 
+#     hist_canvas = []
     
-    return scale
-
-def calc_hEn_sub(hEn: list, hEn_bkg: list, scale: list[float]):
-    """Takes 2 lists of histograms and a list of scales and calculates a new histogram then returns the histograms as a list"""
-    hEn_bkg_sub = []
-    for i in range(numch):
-        hEn_bkg_sub.append(hEn[i]-hEn_bkg[i]*scale[i])
-        hEn_bkg_sub[i].SetDirectory(0)
-
-    return hEn_bkg_sub
-
-def calc_A_LH_det(hEn_bkg_sub: list[ROOT.TH1]) -> tuple[list[float], list[float]]:
-    """Takes a list of histograms and returns a list of A_LH_det and its error dA_LH_det"""
-    N_L_det = []
-    N_H_det = []
-    for i in range(numch):
-        N_L_det.append(hEn_bkg_sub[ch].Integral(E_p - G_p, E_p - 1))
-        N_H_det.append(hEn_bkg_sub[ch].Integral(E_p, E_p + G_p))
+#     for i in range(len(hist)):
+        
+#         hist_canvas.append(ROOT.TCanvas(f"hEn_d{i}", "My Graph", 800, 600))
+#         hist[i].GetXaxis().SetRange(energy - 6 * width, energy + 6 * width)
+#         hist[i].Draw("E1")
+#         hist_canvas[i].Update()
     
-        try:     
-            A_LH_det.append((N_L_det[i] - N_H_det[i]) / (N_L_det[i] + N_H_det[i]))
-            dA_LH_det.append(2 * ROOT.TMath.Sqrt((N_L_det[i] * N_L_det[i] * N_H_det[i] + N_L_det[i] * N_H_det[i] * N_H_det[i])) / (
-                N_L_det[i] + N_H_det[i]) / (N_L_det[i] + N_H_det[i]))
-        except ZeroDivisionError:
-            A_LH_det.append(0)
-            dA_LH_det.append(0)
-
-    return A_LH_det, dA_LH_det
-
-
-
-
 
 # for ch in range(numch):
     
@@ -289,13 +367,29 @@ def calc_A_LH_det(hEn_bkg_sub: list[ROOT.TH1]) -> tuple[list[float], list[float]
 
 # file_output.Close()
 
+  # canvas = []
+    # for i in range(len(hEn_sub)):
+    #     canvas.append(ROOT.TCanvas(f"hEn_d{i}", "My Graph", 800, 600))
+    #     hEn_sub[i].Draw("E1")
+    #     canvas[i].Update()
 
 if __name__=="__main__":
 
     input_file = "stage0_output_NaI_1.root"
+    E_p = 13.6
+    G_p = 0.5
 
-    det_pos = get_det_map(input_file)
+    det_angle = get_det_angle(input_file)
 
     hEn = get_hist(input_file,"hEn_gated_FA")
     hEn_bkg = get_hist(input_file,"hEn_gated_FA_bkg")
     hEgam = get_hist(input_file,"hEgam")
+
+    scale = calc_scale(hEn_bkg)
+
+    hEn_sub = calc_hEn_sub(hEn, hEn_bkg, scale)
+
+    A_LH_det, dA_LH_det = get_A_LH_det(hEn_sub, E_p, G_p)
+    A_LH_angle, dA_LH_det = get_A_LH_angle(hEn_sub, det_angle, E_p, G_p)
+    print(A_LH_det)
+    print(A_LH_angle)
