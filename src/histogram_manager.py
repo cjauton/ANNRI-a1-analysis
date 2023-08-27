2ccda206e4ee7c88bf00b8b3e5b2ca7c399c64cc
cjauton <126740758+cjauton@users.noreply.github.com>
2023-08-27 23:02:00 +0900

    added plotting and repr

--- src/histogram_manager.py
+++ src/histogram_manager.py
@@ -3,9 +3,9 @@ import ROOT
 class HistogramManager:
     def __init__(self, filename):
         self.filename = filename
-        self.histograms = self.load_histograms_from_file()
+        self._histograms = self._load_histograms_from_file()
 
-    def load_histograms_from_file(self):
+    def _load_histograms_from_file(self):
         """Takes a root file named filename and returns a dictionary of histograms"""
 
         def recursive_read(hist_dict: dict, dir: ROOT.TDirectoryFile):
@@ -29,9 +29,104 @@ class HistogramManager:
             recursive_read(hist_dicts,infile)
 
         return hist_dicts
+    
+    @property
+    def histograms(self):
+        """Lazy load histograms when they are accessed."""
+        if self._histograms is None:
+            self._histograms = self._load_histograms_from_file()
+        return self._histograms
+    
+    def write(self, filename):
+        """Takes a dictionary of histograms and writes them to a root file named filename"""
+
+        def recursive_write(hist_dict, dir):
+            """Takes a dictionary and recursively writes to a TDirectoryFile"""
+
+            for key, hist in hist_dict.items():
+                dir.cd()
+                
+                if type(hist) is dict:
+                    subdir = dir.mkdir(key)
+                    subdir.cd()
+                    recursive_write(hist, subdir)
+
+                else:
+                    hist.Write()
+
+        with ROOT.TFile(filename, 'recreate') as outfile:
+            recursive_write(self.histograms, outfile)
+            
+    def _recursive_search(self, key, dictionary):
+        """
+        Recursively searches for the histogram with the given key.
+
+        Parameters:
+            key (str): The key of the histogram to find.
+            dictionary (dict): The dictionary or sub-dictionary to search within.
+
+        Returns:
+            obj (ROOT.TH1 or dict or None): The histogram or directory if found, otherwise None.
+        """
+        if key in dictionary:
+            return dictionary[key]
+        for k, v in dictionary.items():
+            if isinstance(v, dict):
+                result = self._recursive_search(key, v)
+                if result:
+                    return result
+        return None          
 
 
-    def _repr_tree(self, dictionary, indent=4):
+    def plot(self, key, x_range=None):
+        """
+        Plots the histogram(s) associated with the given key. 
+        
+        Parameters:
+            key (str): The key of the histogram or directory to plot.
+            x_range (tuple, optional): A tuple of (xmin, xmax) to set the x-axis range.
+
+        Returns:
+            list: A list of ROOT.TCanvas objects.
+        """
+        # Retrieve the associated object using recursive search
+        obj = self._recursive_search(key, self.histograms)
+        canvases = []
+
+        # Check if the histogram or directory was found
+        if not obj:
+            print(f"No histogram or directory found with key: {key}")
+            return canvases
+
+        def draw_histogram(hist, canvas_name):
+            """Helper function to draw a histogram on a canvas"""
+            canvas = ROOT.TCanvas(canvas_name, f"Histogram Canvas - {canvas_name}", 800, 600)
+            if x_range:
+                hist.GetXaxis().SetRangeUser(*x_range)
+            hist.Draw()
+            canvas.Update()
+            canvases.append(canvas)
+
+        # If it's a single histogram, draw it on a canvas and append to the list
+        if isinstance(obj, ROOT.TH1):
+            draw_histogram(obj, f"canvas_{key}")
+
+        # If it's a directory, draw each histogram on its own canvas and append to the list
+        elif isinstance(obj, dict):
+            for name, hist in obj.items():
+                if isinstance(hist, ROOT.TH1):
+                    draw_histogram(hist, f"canvas_{name}")
+                elif isinstance(hist, dict):  # nested directories
+                    canvases.extend(self.plot(name, x_range=x_range))
+
+        else:
+            print(f"Key '{key}' does not correspond to a known object type.")
+
+        return canvases
+
+
+
+    def _repr_tree(self, dictionary):
         rows = []
         total_objects = 0
 
@@ -41,12 +136,14 @@ class HistogramManager:
                 representation = f"📂 {first_object_type}"  # Folder icon represents a directory
                 total_objects += len(value)
                 rows.append((key, representation))
+                
             else:
                 total_objects += 1
+                
                 if type(value).__name__ == "TGraph":
-                    rows.append((key, "📈 TGraph"))  # Using the chart increasing emoji for TGraph
+                    rows.append((key, "📈 TGraph")) 
+                    
                 else:
-                    # Using the bar chart emoji for standalone histograms
                     rows.append((key, f"📊 {type(value).__name__}"))
 
         # Sorting entries: directories first, then standalone histograms.
