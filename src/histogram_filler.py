7d5099ae628ee2b03bd9bc780bd375ca03b9cbdc
cjauton <126740758+cjauton@users.noreply.github.com>
2024-06-03 13:19:49 +0900

    formatted

--- src/histogram_filler.py
+++ src/histogram_filler.py
@@ -5,17 +5,21 @@ from dataclasses import dataclass, field
 import utils
 import histogram_manager as hm
 
+
 @dataclass
 class Config:
     """Configuration for histogram filling."""
+
     numch: int
     fp_length: float
     data_format: str
     paths: dict
 
+
 @dataclass
 class HistogramDefinition:
     """Definition of the histograms to be created."""
+
     xaxis: str
     yaxis: str
     bins: dict
@@ -24,9 +28,11 @@ class HistogramDefinition:
     gate: str
     col: str
 
+
 @dataclass
 class HistogramFiller:
     """Fills histograms from a ROOT RDataFrame based on given configurations."""
+
     df: ROOT.RDataFrame
     config_path: str
     hist_def_path: str
@@ -36,37 +42,41 @@ class HistogramFiller:
     calib_slope: np.ndarray = field(init=False)
     calib_offset: np.ndarray = field(init=False)
     ACTIVE_CH_LIST: list = field(init=False)
-    
+
     instance_num = 0
 
     def __post_init__(self):
         """Initializes the HistogramFiller instance."""
         self.instance_id = __class__.instance_num
         __class__.instance_num += 1
-     
+
         self.config = self.load_config(self.config_path)
         self.hist_def = self.load_histogram_definitions(self.hist_def_path)
-        self.det_map = utils.load_det_map(self.config.paths['run_info'])
+        self.det_map = utils.load_det_map(self.config.paths["run_info"])
         self.ACTIVE_CH_LIST = self.det_map["active"]
 
-        self.calib_slope, self.calib_offset = self.extract_calib_from_csv(self.config.paths['calib'], self.config.numch)
-        
+        self.calib_slope, self.calib_offset = self.extract_calib_from_csv(
+            self.config.paths["calib"], self.config.numch
+        )
+
         calib_slope_str = "{" + ",".join(map(str, self.calib_slope.tolist())) + "}"
         calib_offset_str = "{" + ",".join(map(str, self.calib_offset.tolist())) + "}"
-        
-        ROOT.gInterpreter.Declare(f"""
+
+        ROOT.gInterpreter.Declare(
+            f"""
         std::vector<double> calib_slope_{self.instance_id} = {calib_slope_str};
         std::vector<double> calib_offset_{self.instance_id} = {calib_offset_str};
-        """)
+        """
+        )
 
     def load_config(self, config_path):
         """Loads configuration from a TOML file."""
         config_dict = utils.load_toml_to_dict(config_path)
         return Config(
-            numch=config_dict['general']['numch'],
-            fp_length=config_dict['general']['fp_length'],
-            data_format=config_dict['general']['data_format'],
-            paths=config_dict['paths']
+            numch=config_dict["general"]["numch"],
+            fp_length=config_dict["general"]["fp_length"],
+            data_format=config_dict["general"]["data_format"],
+            paths=config_dict["paths"],
         )
 
     def load_histogram_definitions(self, hist_def_path):
@@ -74,14 +84,15 @@ class HistogramFiller:
         hist_def_dict = utils.load_toml_to_dict(hist_def_path)
         return {
             key: HistogramDefinition(
-                xaxis=val['xaxis'],
-                yaxis=val['yaxis'],
-                bins=val['bins'],
-                all=val['all'],
-                name=val['name'],
-                gate=val['gate'],
-                col=val['col']
-            ) for key, val in hist_def_dict['histograms'].items()
+                xaxis=hist_def_dict["column"][val["col"]]["xaxis"],
+                yaxis=hist_def_dict["column"][val["col"]]["yaxis"],
+                bins=hist_def_dict["column"][val["col"]]["bins"],
+                all=val["all"],
+                name=key,
+                gate=val["gate"],
+                col=val["col"],
+            )
+            for key, val in hist_def_dict["histograms"].items()
         }
 
     def extract_calib_from_csv(self, filename, numch):
@@ -89,7 +100,7 @@ class HistogramFiller:
         calib_slope = np.ones(numch)
         calib_offset = np.zeros(numch)
 
-        with open(filename, 'r') as file:
+        with open(filename, "r") as file:
             reader = csv.reader(file)
             next(reader)
             for row in reader:
@@ -101,12 +112,17 @@ class HistogramFiller:
 
     def define_columns(self):
         """Defines new columns in the DataFrame based on calibration data."""
-        self.df = (self.df.Define("tof_ns", "tof*10.0")
-                          .Alias("tof_10ns", "tof")
-                          .Define("tof_mus", "tof_ns/1000.0+0.0000001")
-                          .Define("En", "pow((72.3*21.5/(tof_ns/1000.0)),2)+0.00001")
-                          .Define("Egam", f"PulseHeight*calib_slope_{self.instance_id}[detector]+calib_offset_{self.instance_id}[detector]")
-                          .Define('Egam_rounded', "std::round(Egam)*1.0"))
+        self.df = (
+            self.df.Define("tof_ns", "tof*10.0")
+            .Alias("tof_10ns", "tof")
+            .Define("tof_mus", "tof_ns/1000.0+0.0000001")
+            .Define("En", "pow((72.3*21.5/(tof_ns/1000.0)),2)+0.00001")
+            .Define(
+                "Egam",
+                f"PulseHeight*calib_slope_{self.instance_id}[detector]+calib_offset_{self.instance_id}[detector]",
+            )
+            .Define("Egam_rounded", "std::round(Egam)*1.0")
+        )
 
     def filter_active_channels(self):
         """Filters out inactive channels from the DataFrame."""
@@ -117,20 +133,20 @@ class HistogramFiller:
     def create_hist_model_dict(self):
         """*DEPRECATED* Creates a dictionary of histogram models."""
         hist_model_dict = {}
-        
+
         function_map = {
-                "En": utils.get_xbins_En,
-                "Egam": utils.get_xbins_Egam,
-                "tof_mus": utils.get_xbins_tof_mus,
-                "tof_ns": utils.get_xbins_tof_ns,
-                "tof_10ns": utils.get_xbins_tof_10ns,
-            }
-        
+            "En": utils.get_xbins_En,
+            "Egam": utils.get_xbins_Egam,
+            "tof_mus": utils.get_xbins_tof_mus,
+            "tof_ns": utils.get_xbins_tof_ns,
+            "tof_10ns": utils.get_xbins_tof_10ns,
+        }
+
         for key, hist_def in self.hist_def.items():
             xaxis = hist_def.xaxis
             yaxis = hist_def.yaxis
             bins = hist_def.bins
-            down, up, rebin = bins['down'], bins['up'], bins['rebin']
+            down, up, rebin = bins["down"], bins["up"], bins["rebin"]
 
             func = function_map.get(hist_def.col)
             if func:
@@ -140,18 +156,22 @@ class HistogramFiller:
                     xbins = func(down, up, rebin)
             else:
                 raise ValueError(f"Unknown column: {hist_def.col}")
-                
+
             if hist_def.all:
                 name = hist_def.name
                 title = f"{name};{xaxis};{yaxis}"
-                hist_model_dict[key] = ROOT.RDF.TH1DModel(name, title, xbins.len(), xbins)
+                hist_model_dict[key] = ROOT.RDF.TH1DModel(
+                    name, title, xbins.len(), xbins
+                )
             else:
                 hist_model_dict[key] = {}
                 for ch in range(self.config.numch):
                     name = f"{hist_def.name}_d{ch}"
                     title = f"{name};{xaxis};{yaxis}"
-                    scale = self.calib_slope[ch] if 'hEgam' in name else 1
-                    hist_model_dict[key][name] = ROOT.RDF.TH1DModel(name, title, xbins.len(), xbins * scale)
+                    scale = self.calib_slope[ch] if "hEgam" in name else 1
+                    hist_model_dict[key][name] = ROOT.RDF.TH1DModel(
+                        name, title, xbins.len(), xbins * scale
+                    )
 
         return hist_model_dict
 
@@ -162,7 +182,7 @@ class HistogramFiller:
             gate = hist_def.gate
             name = hist_def.name
             df_gate = self.df if gate == "" else self.df.Filter(gate)
-            
+
             if hist_def.all:
                 df_dict[key] = df_gate
             else:
@@ -177,13 +197,15 @@ class HistogramFiller:
         hist_dict = {}
         for key, hist_model in hist_model_dict.items():
             col = self.hist_def[key].col
-            
+
             if self.hist_def[key].all:
                 hist_dict[key] = df_dict[key].Histo1D(hist_model, col)
             else:
                 hist_dict[key] = {}
                 for ch in range(self.config.numch):
-                    hist_dict[key][f"{key}_d{ch}"] = df_dict[key][f"{key}_d{ch}"].Histo1D(hist_model[f"{key}_d{ch}"], col)
+                    hist_dict[key][f"{key}_d{ch}"] = df_dict[key][
+                        f"{key}_d{ch}"
+                    ].Histo1D(hist_model[f"{key}_d{ch}"], col)
 
         return hist_dict
 
@@ -199,8 +221,8 @@ class HistogramFiller:
             col = hist_def.col
             gate = hist_def.gate
             bins = hist_def.bins
-            down, up, rebin = bins['down'], bins['up'], bins['rebin']
-            
+            down, up, rebin = bins["down"], bins["up"], bins["rebin"]
+
             if hist_def.col == "En":
                 xbins = utils.get_xbins_En(down, up, self.config.fp_length, rebin)
             elif hist_def.col == "Egam":
@@ -217,29 +239,31 @@ class HistogramFiller:
 
             if hist_def.all:
                 title = f"{name};{xaxis};{yaxis}"
-                hist_model_all = ROOT.RDF.TH1DModel(name, title, len(xbins)-1, xbins)
+                hist_model_all = ROOT.RDF.TH1DModel(name, title, len(xbins) - 1, xbins)
                 hist_all = df_gate_all.Histo1D(hist_model_all, col)
                 hist_dict[key] = hist_all
             else:
                 hist_dict[key] = {}
                 for ch in range(self.config.numch):
                     title = f"{name}_d{ch};{xaxis};{yaxis}"
-                    scale = self.calib_slope[ch] if 'hEgam' in name else 1
-                    hist_model_ch = ROOT.RDF.TH1DModel(f"{name}_d{ch}", title, len(xbins)-1, xbins * scale)
+                    scale = self.calib_slope[ch] if "hEgam" in name else 1
+                    hist_model_ch = ROOT.RDF.TH1DModel(
+                        f"{name}_d{ch}", title, len(xbins) - 1, xbins * scale
+                    )
                     hist_ch = df_gate_ch[ch].Histo1D(hist_model_ch, col)
                     hist_dict[key][f"{name}_d{ch}"] = hist_ch
 
         return hist_dict
-    
+
     def create_hm_from_df(self):
         """Creates a HistogramManager instance from the DataFrame."""
         return hm.HistogramManager(self.create_hist_dict_from_df())
 
     def read_rawroot_to_dict(self, file_name, filter_active=False):
-        """Reads a raw ROOT file and converts it to a dictionary of histograms."""
+        """*DEPRECATED* Reads a raw ROOT file and converts it to a dictionary of histograms."""
         ROOT.EnableImplicitMT()
         df = ROOT.RDataFrame(file_name)
-        
+
         if filter_active:
             self.filter_active_channels()
 
@@ -254,23 +278,36 @@ class HistogramFiller:
             col = hist_def.col
             gate = hist_def.gate
             bins = hist_def.bins
-            down, up, N, var = bins['down'], bins['up'], bins['N'], bins['var']
+            down, up, N, var = bins["down"], bins["up"], bins["N"], bins["var"]
 
-            df_gate_ch = [self.df.Filter(gate).Filter(f"detector == {ch}") for ch in range(self.config.numch)] if gate else df_ch
+            df_gate_ch = (
+                [
+                    self.df.Filter(gate).Filter(f"detector == {ch}")
+                    for ch in range(self.config.numch)
+                ]
+                if gate
+                else df_ch
+            )
 
             if hist_def.all:
                 title = f"{name};{xaxis};{yaxis}"
                 xbins = utils.get_xbins(N, down, up, var, self.config.fp_length)
                 hist_model_all = ROOT.RDF.TH1DModel(name, title, N, xbins)
-                hist_all = self.df.Filter(gate).Histo1D(hist_model_all, col) if gate else self.df.Histo1D(hist_model_all, col)
+                hist_all = (
+                    self.df.Filter(gate).Histo1D(hist_model_all, col)
+                    if gate
+                    else self.df.Histo1D(hist_model_all, col)
+                )
                 hist_dict[key] = hist_all
             else:
                 hist_dict[key] = {}
                 for ch in range(self.config.numch):
                     title = f"{name}_d{ch};{xaxis};{yaxis}"
                     xbins = utils.get_xbins(N, down, up, var, self.config.fp_length)
-                    scale = self.calib_slope[ch] if 'hEgam' in name else 1
-                    hist_model_ch = ROOT.RDF.TH1DModel(f"{name}_d{ch}", title, N, xbins * scale)
+                    scale = self.calib_slope[ch] if "hEgam" in name else 1
+                    hist_model_ch = ROOT.RDF.TH1DModel(
+                        f"{name}_d{ch}", title, N, xbins * scale
+                    )
                     hist_ch = df_gate_ch[ch].Histo1D(hist_model_ch, col)
                     hist_dict[key][f"{name}_d{ch}"] = hist_ch
 
